f053b7bb1a780ee741189f0d58ab2e6476efd368
Sean King <r.sean.king@gmail.com>
2018-06-01 13:44:31 -0400

    Introduce .gitignore

--- spring-authentication-service/src/main/java/com/rseanking/authentication/Application.java
+++ spring-authentication-service/src/main/java/com/rseanking/authentication/Application.java
@@ -14,9 +14,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableResourceServer
 @EnableAuthorizationServer
 public class Application {
+
+	@RequestMapping(value = "/user", produces = "application/json")
+	private Map<String, Object> user(OAuth2Authentication user) {
+		final Map<String, Object> userInfo = new HashMap<>();
+
+		userInfo.put("user", user.getUserAuthentication().getPrincipal());
+		userInfo.put("authorities", authorityListToSet(user.getUserAuthentication().getAuthorities()));
+
+		return userInfo;
+	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
