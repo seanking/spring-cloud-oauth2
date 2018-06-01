d2557c0bfb47bf67a59a02b91cfae88bd33e8077
Sean King <r.sean.king@gmail.com>
2018-06-01 15:13:42 -0400

    Extract users endpoint from the application class

--- spring-authentication-service/src/main/java/com/rseanking/authentication/Application.java
+++ spring-authentication-service/src/main/java/com/rseanking/authentication/Application.java
@@ -1,37 +1,18 @@
 package com.rseanking.authentication;
 
-import static org.springframework.security.core.authority.AuthorityUtils.authorityListToSet;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
-import org.springframework.security.oauth2.provider.OAuth2Authentication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import com.rseanking.user.UserRepository;
 
 @SpringBootApplication
-@RestController
 @EnableResourceServer
 @EnableAuthorizationServer
 @EnableMongoRepositories(basePackageClasses= {UserRepository.class})
 public class Application {
-
-	@RequestMapping(value = "/user", produces = "application/json")
-	private Map<String, Object> user(OAuth2Authentication user) {
-		final Map<String, Object> userInfo = new HashMap<>();
-
-		userInfo.put("user", user.getUserAuthentication().getPrincipal());
-		userInfo.put("authorities", authorityListToSet(user.getUserAuthentication().getAuthorities()));
-
-		return userInfo;
-	}
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(Application.class, args);
---
+++ spring-authentication-service/src/main/java/com/rseanking/authentication/users/UsersController.java
@@ -0,0 +1,25 @@
+package com.rseanking.authentication.users;
+
+import static org.springframework.security.core.authority.AuthorityUtils.authorityListToSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersController {
+
+	@RequestMapping(value = "/user", produces = "application/json")
+	private Map<String, Object> user(OAuth2Authentication user) {
+		final Map<String, Object> userInfo = new HashMap<>();
+
+		userInfo.put("user", user.getUserAuthentication().getPrincipal());
+		userInfo.put("authorities", authorityListToSet(user.getUserAuthentication().getAuthorities()));
+
+		return userInfo;
+	}
+
+}
