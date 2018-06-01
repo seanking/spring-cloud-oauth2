2d1757c8c4b498024901ebe5b27db6c8c49be6b3
Sean King <r.sean.king@gmail.com>
2018-06-01 14:19:46 -0400

    Look up accounts using the user collection with mongo

---
+++ spring-authentication-service/src/main/java/com/rseanking/authentication/details/DefaultUserDetailsService.java
@@ -0,0 +1,28 @@
+package com.rseanking.authentication.details;
+
+import static java.lang.String.format;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.rseanking.user.User;
+import com.rseanking.user.UserRepository;
+
+public class DefaultUserDetailsService implements UserDetailsService {
+	@Autowired
+	private UserRepository repo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		final User user = repo.findByUsername(username);
+		if(user != null) {
+			return new UserDetailsAdapter(user);
+		}
+		
+		final String errorMessage = format("Couldn't find the user for username: %s", username);
+		throw new UsernameNotFoundException(username);
+	}
+
+}
---
+++ spring-authentication-service/src/test/java/com/rseanking/authentication/details/DefaultUserDetailsServiceTest.java
@@ -0,0 +1,48 @@
+package com.rseanking.authentication.details;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.*;
+import static org.mockito.BDDMockito.given;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.BDDMockito;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.rseanking.user.User;
+import com.rseanking.user.UserRepository;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DefaultUserDetailsServiceTest {
+	@Mock
+	private UserRepository repo;
+	@InjectMocks
+	private DefaultUserDetailsService uut;
+	
+	
+	@Test
+	public void shouldLoadUserByUsername() {
+		// Given
+		final User user = new User();
+		user.setUsername("testusername");
+		user.setPasword("testpassword");
+		
+		given(repo.findByUsername(user.getUsername())).willReturn(user);
+		
+		// When
+		final UserDetailsAdapter userDetails = (UserDetailsAdapter) uut.loadUserByUsername(user.getUsername());
+		
+		// Then 
+		assertThat(userDetails.getUser()).isEqualTo(user);
+	}
+	
+	@Test(expected=UsernameNotFoundException.class)
+	public void shouldNotLoadUserByUsername() {
+		final UserDetailsAdapter userDetails = (UserDetailsAdapter) uut.loadUserByUsername("invalidname");
+	}
+
+}
