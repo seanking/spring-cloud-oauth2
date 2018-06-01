5d7de337cb6b940108126b216b5a936bc2ee06f5
Sean King <r.sean.king@gmail.com>
2018-06-01 14:19:20 -0400

    Introduce an adapter for the User and UserDetails

---
+++ spring-authentication-service/src/test/java/com/rseanking/authentication/details/UserDetailAdapterTest.java
@@ -0,0 +1,32 @@
+package com.rseanking.authentication.details;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.Test;
+
+import com.rseanking.user.User;
+
+public class UserDetailAdapterTest {
+	
+	@Test
+	public void shouldAdaptUser() {
+		// Given
+		final User user = new User();
+		user.setUsername("testusername");
+		user.setPasword("testpassword");
+		
+		// When
+		final UserDetailsAdapter details = new UserDetailsAdapter(user);
+		
+		// Then
+		assertThat(details.getUser()).isEqualTo(user);
+		assertThat(details.getUsername()).isEqualTo(user.getUsername());
+		assertThat(details.getPassword()).isEqualTo(user.getPasword());
+		assertThat(details.getAuthorities()).isNull();
+		assertThat(details.isAccountNonExpired()).isTrue();
+		assertThat(details.isAccountNonLocked()).isTrue();
+		assertThat(details.isCredentialsNonExpired()).isTrue();
+		assertThat(details.isEnabled()).isTrue();
+	}
+	
+}
