6f3acb7c2587382e12b22f84b0bbad6898e212ac
Sean King <r.sean.king@gmail.com>
2018-06-02 13:53:59 -0400

    Replace the UserDetailAdapter with a builder proviced by spring

--- spring-authentication-service/src/main/java/com/rseanking/authentication/details/DefaultUserDetailsService.java
+++ spring-authentication-service/src/main/java/com/rseanking/authentication/details/DefaultUserDetailsService.java
@@ -1,6 +1,9 @@
 package com.rseanking.authentication.details;
 
 import static java.lang.String.format;
+import static java.util.stream.Collectors.toList;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,8 +22,13 @@ public class DefaultUserDetailsService implements UserDetailsService {
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		final User user = repo.findByUsername(username);
+		
 		if(user != null) {
-			return new UserDetailsAdapter(user);
+			final List<String> roles = user.getRoles().stream().map(r -> r.name()).collect(toList());
+			return org.springframework.security.core.userdetails.User.withUsername(user.getUsername())
+				.password(user.getPasword())
+				.roles(roles.toArray(new String[roles.size()]))
+				.build();
 		}
 		
 		final String errorMessage = format("Couldn't find the user for username: %s", username);
